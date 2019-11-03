d896652fee482c6dcd95cc11a89f57bf385a4226
Jared Gray <jaredgray.dev@gmail.com>
2019-11-03 15:41:19 -0700

    Add basic styling and links to stackoverflow and github page

--- src/Section.tsx
+++ src/Section.tsx
@@ -4,13 +4,30 @@ import { faGithub, faStackOverflow } from "@fortawesome/free-brands-svg-icons";
 
 const Section = (): ReactElement => {
   return (
-    <div>
-      <header>
-        <h2>Hello I'm Jared Gray</h2>
-        <FontAwesomeIcon icon={faStackOverflow} />
-        <FontAwesomeIcon icon={faGithub} />
-      </header>
-    </div>
+    <section className="hero is-fullheight is-dark is-bold">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title is-2">Hello I'm Jared Gray</h1>
+          <br />
+          <h2 className="subtitle is-3">
+            <a
+              href="https://stackoverflow.com/users/4735914/jdgray"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faStackOverflow} />
+            </a>{" "}
+            <a
+              href="https://github.com/jd-gray"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </h2>
+        </div>
+      </div>
+    </section>
   );
 };
 
