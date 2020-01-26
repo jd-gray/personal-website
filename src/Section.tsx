462a9a49e6cf4a90d58614feee3e18aaece97f3a
Jared Gray <jared@jaredgray.us>
2020-01-26 09:02:22 -0700

    Add strava badge to app

--- src/Section.tsx
+++ src/Section.tsx
@@ -2,6 +2,25 @@ import React, { ReactElement } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faStackOverflow } from "@fortawesome/free-brands-svg-icons";
 
+const stravaLinkStyles = {
+  display: "inline-block",
+  backgroundColor: "#FC5200",
+  color: "#fff",
+  padding: "5px 10px 5px 30px",
+  fontSize: "11px",
+  fontFamily: "Helvetica, Arial, sans-serif",
+  textDecoration: "none",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "10px center",
+  borderRadius: "3px",
+  backgroundImage: "url('http://badges.strava.com/logo-strava-echelon.png')"
+};
+
+const stravaImgStyles = {
+  marginLeft: "2px",
+  verticalAlign: "text-bottom"
+};
+
 const Section = (): ReactElement => {
   return (
     <section className="hero is-fullheight is-dark is-bold">
@@ -9,7 +28,7 @@ const Section = (): ReactElement => {
         <div className="container has-text-centered">
           <h1 className="title is-2">Hello I'm Jared Gray</h1>
           <br />
-          <h2 className="subtitle is-3">
+          <div className="subtitle is-3">
             <a
               href="https://stackoverflow.com/users/4735914/jdgray"
               target="_blank"
@@ -24,7 +43,21 @@ const Section = (): ReactElement => {
             >
               <FontAwesomeIcon icon={faGithub} />
             </a>
-          </h2>
+          </div>
+          <a
+            style={stravaLinkStyles}
+            href="http://strava.com/athletes/jaredgray"
+            target="_clean"
+          >
+            Follow me on
+            <img
+              src="http://badges.strava.com/logo-strava.png"
+              alt="Strava"
+              style={stravaImgStyles}
+              height={13}
+              width={51}
+            />
+          </a>
         </div>
       </div>
     </section>
